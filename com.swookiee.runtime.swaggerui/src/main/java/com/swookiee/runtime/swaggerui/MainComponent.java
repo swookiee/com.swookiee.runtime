07a5c65b729c46d08473424e16e7efe9d2a56849
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-03-22 16:55:26 +0100

    moved swagger files to tools subfolder that github statistics ignore them and updated to newer swookiee avatar

--- com.swookiee.runtime.swaggerui/src/main/java/com/swookiee/runtime/swaggerui/MainComponent.java
+++ com.swookiee.runtime.swaggerui/src/main/java/com/swookiee/runtime/swaggerui/MainComponent.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class MainComponent {
 
     static final String SWAGGER_ALIAS = "/swagger";
-    private static final String RESOURCES_FOLDER = "/resources";
+    private static final String RESOURCES_FOLDER = "/tools";
     private static final Logger logger = LoggerFactory.getLogger(MainComponent.class);
 
     private HttpService httpService;
