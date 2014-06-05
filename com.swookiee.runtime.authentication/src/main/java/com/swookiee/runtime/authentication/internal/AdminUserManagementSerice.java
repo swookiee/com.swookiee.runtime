9601e6c62a2a4012c1e162f900fcf5b360293308
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-05 12:03:30 +0200

    Added default EPL header to groovy and java files

--- com.swookiee.runtime.authentication/src/main/java/com/swookiee/runtime/authentication/internal/AdminUserManagementSerice.java
+++ com.swookiee.runtime.authentication/src/main/java/com/swookiee/runtime/authentication/internal/AdminUserManagementSerice.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Lars Pfannenschmidt and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Lars Pfannenschmidt - initial API and implementation, ongoing development and documentation
+ *******************************************************************************/
+
 package com.swookiee.runtime.authentication.internal;
 
 import java.io.UnsupportedEncodingException;
