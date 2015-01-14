c366dc16b8fea3c9045fabe59386efe9a03e8356
Lars Pfannenschmidt <lars@mobilecologne.de>
2015-01-14 16:12:43 +0100

    Metrics now also get collected on behalf of every resource
    Added also a simple test and multithreading test for the metrics calculations.

--- com.swookiee.runtime.metrics/src/main/java/com/swookiee/runtime/metrics/TimingResourceFilter.java
+++ com.swookiee.runtime.metrics/src/main/java/com/swookiee/runtime/metrics/TimingResourceFilter.java
@@ -14,17 +14,24 @@ package com.swookiee.runtime.metrics;
 import static com.codahale.metrics.MetricRegistry.name;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import org.glassfish.jersey.server.internal.routing.UriRoutingContext;
+import org.glassfish.jersey.server.model.ResourceMethodInvoker;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.slf4j.Logger;
@@ -36,10 +43,14 @@ import com.codahale.metrics.Timer.Context;
 
 @Component
 @Provider
+@Priority(Priorities.HEADER_DECORATOR)
 public class TimingResourceFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(TimingResourceFilter.class);
-    private final ConcurrentMap<ContainerRequestContext, Timer.Context> requestTiming = new ConcurrentHashMap<>();
+
+    private final static String HEADER_FIELD_NAME = "X-Processing-Time";
+
+    private final Map<ContainerRequestContext, Timer.Context> resourceRequestTimers = new ConcurrentHashMap<>();
 
     private MetricRegistry metricRegistry;
     private Timer requestTimer;
@@ -55,25 +66,47 @@ public class TimingResourceFilter implements ContainerRequestFilter, ContainerRe
 
     @Activate
     public void activate() {
+        logger.info("Activate Request Timer!");
         this.requestTimer = this.metricRegistry.timer(name(getClass(), "requests"));
-        logger.info("Request Timer activated!");
+    }
+
+    @Deactivate
+    public void deactivate() {
+        resourceRequestTimers.clear();
+        logger.info("Deactivated Request Timer!");
     }
 
     @Override
     public void filter(final ContainerRequestContext requestContext) throws IOException {
-        this.requestTiming.put(requestContext, this.requestTimer.time());
+        Timer timer = this.metricRegistry.timer(getResourceTimerName(requestContext));
+        this.resourceRequestTimers.put(requestContext, timer.time());
     }
 
     @Override
     public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext)
             throws IOException {
-        final Context context = this.requestTiming.get(requestContext);
+        Context resourceTimer = this.resourceRequestTimers.get(requestContext);
 
-        if (context == null) {
+        if (resourceTimer == null) {
             return;
         }
-        context.stop();
-        this.requestTiming.remove(requestContext);
+
+        long elapsed = resourceTimer.stop();
+        this.requestTimer.update(elapsed, TimeUnit.NANOSECONDS);
+        responseContext.getHeaders().putSingle(HEADER_FIELD_NAME, elapsed);
+        this.resourceRequestTimers.remove(requestContext);
+
     }
 
+    public String getResourceTimerName(ContainerRequestContext requestContext) {
+        try {
+            UriRoutingContext routingContext = (UriRoutingContext) requestContext.getUriInfo();
+            ResourceMethodInvoker invoker = (ResourceMethodInvoker) routingContext.getInflector();
+            Class<?> clazz = invoker.getResourceClass();
+            Method method = invoker.getResourceMethod();
+            return name(clazz, method.getName());
+        } catch (Exception ex) {
+            return "undefined";
+        }
+    }
 }
